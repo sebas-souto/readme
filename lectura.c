bee138389b8156b2b9a7e4e5437fd25c53ae696f
sebas-souto <souto.sebastianl@gmail.com>
2017-04-04 17:45:18 +0200

    Update lectura.c

--- lectura.c
+++ lectura.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*NOTA: NO USA EL FSCANF, LO HABRIA QUE MODIFICAR.*/
-int main()
-{
-	FILE *archivo;
-	int caracter;
-	char array[256];
-	int num=0;
-	
-	archivo = fopen("lectores.txt","r");
-	
-	if (archivo == NULL) {
-            printf("\nError de apertura del archivo. \n\n");
-        }
-        else {
-            printf("\nEl contenido del archivo de prueba es \n\n");
-            
-             while((caracter = fgetc(archivo)) != EOF)  {
-            
-           /* while((caracter = fgetc(archivo)) != EOF)  {
-		printf("%c",caracter);
-	    }*/
-	    fscanf('archivo',":%i:%s:",&num,&array);
-	    printf("%i %s\n",num,array);
-			}
-	
-        }
-        fclose(archivo);
-	return 0;
+
+ 
+int main ( int argc, char **argv ){
+ 	FILE *fp;
+ 	
+ 	char buffer[256];
+ 	int num=0;
+ 	fp = fopen ( "lectores.txt", "r" );
+ 	
+ 	fscanf(fp, ":%i:%s:" ,&num,buffer);
+ 	printf("%i %s",num,buffer);
+ 	
+ 	fclose ( fp );
+ 	
+ 	return 0;
 }
