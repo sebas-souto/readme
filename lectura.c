37a79192e1637e1a7909147071d0b43166f410d8
sebas-souto <souto.sebastianl@gmail.com>
2017-04-05 18:10:57 +0200

    Update lectura.c

--- lectura.c
+++ lectura.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
- 
-int main ( int argc, char **argv ){
- 	FILE *fp;
- 	
- 	char buffer[256];
- 	int num=0;
- 	fp = fopen ( "lectores.txt", "r" );
- 	
- 	fscanf(fp, ":%i:%s:" ,&num,buffer);
- 	printf("%i %s",num,buffer);
- 	
- 	fclose ( fp );
- 	
- 	return 0;
+int main (int argc, char *argv[]){
+    int num=0;
+    char nombre[256];
+    char apellido[256];
+	int i=0;
+    FILE *archivo;
+    archivo = fopen("lectores.txt", "r");
+    while(i!=5){
+	
+	fscanf(archivo," :%i:%s %s:",&num,&nombre,&apellido);
+	printf(" %i %s %s\n",num,nombre,apellido);
+	i++;
 }
+    fclose(archivo);
+
+    }
+
