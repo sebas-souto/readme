f0088e2feefd3444ea31fa8df6d707d0720d579a
avg333 <avg333@hotmail.com>
2017-04-12 18:40:33 +0200

    Update and rename Calcular_Nota.c to Calcular_Nota_FINAL.c

--- Calcular_Nota_FINAL.c
+++ Calcular_Nota_FINAL.c
@@ -1,30 +1,29 @@
 //Calcular nota
 #include <stdio.h>
-void calcular_nota();
+void calcular_nota(int);
 
 int main(){
-	calcular_nota();
+	calcular_nota(15);
 	return 0;
 }
 
-void calcular_nota(){
-	int id,votantes,ULT_ID=15;
+void calcular_nota(int tamanio){
+	int id,votantes,ULT_ID=tamanio;	//ULT_ID DEBE SER EL VALOR DE LA ULTIMA ID
 	float votos;
-	
-	//ULT_ID DEBE SER EL VALOR DE LA ULTIMA ID
-	//SE USA 15 COMO EJEMPLO
+	char cadena[100];
 	
 	do{
 		printf("\nEscribe la signatura: ");
-		scanf(" %i",&id);
+		fgets(cadena,sizeof(cadena),stdin);
+		id=atoi(cadena);
 		if (id<1 || id>ULT_ID)
 			printf("\nValor seleccionado incorrecto\n");
 	} while (id<1 || id>ULT_ID);
 	
 	//DECIDE DE QUE ID SE CALCULA LA NOTA  		SIGNATURA=ID	
-//	votos=27;votantes=5;
-	//VOTANTES DEBE SER = AL VOTANTES DE ESA ID
-	//VOTOS DEBE SER = AL VOTOS DE ESA ID
+
+	votos=refe[id-1].votos;
+	votantes=refe[id-1].votantes;
 		
 	printf("\nNumero de votos:%i\n",votantes);	
 	printf("Nota media:%.2f",votos/votantes);
