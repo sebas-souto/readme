e80f7c736657a70670c8a108f9ece37df5b25c09
avg333 <avg333@hotmail.com>
2017-04-13 16:12:25 +0200

    Add files via upload

---
+++ confirmar.c
@@ -0,0 +1,30 @@
+//CONFIRMAR
+#include <stdio.h>
+
+int main(){
+	int comprobar=comprobacion();
+	if (comprobar==1)
+		printf("Los txt estan OK");
+	return 0;
+}
+
+int comprobacion(){
+	FILE *pf;
+	pf = fopen("lectores.txt","r");
+	if (pf==NULL){		//SI NO EXISTE EL TXT LO CREA
+		printf("lectores.txt no existe y se ha creado\n");
+		pf = fopen("lectores.txt","w");}
+	else if (1==0){		//SI EXISTE Y NO TIENE EL FORMATO CORRECTO SE CIERRA
+		printf("Linea mal formada en el fichero(lectores.txt)");
+		return 0;}
+		
+	FILE *pf2;
+	pf2 = fopen("referencias.txt","r");
+	if (pf2==NULL){ 	//SI NO EXISTE EL TXT LO CREA
+		printf("referencias.txt no existe y se ha creado\n");
+		pf = fopen("referencias.txt","w");}
+	else if(1==0){		//SI EXISTE Y NO TIENE EL FORMATO CORRECTO SE CIERRA
+		printf("Linea mal formada en el fichero(referencias.txt)");
+		return 0;}
+	return 1;
+}
